17972e3c5a1ad66f21af6ab9a62f6cb853012906
srinath99 <srinath99@gmail.com>
2025-01-31 16:47:14 -0600

    Added option to filter cards by Shelf

--- __init__.py
+++ __init__.py
@@ -21,7 +21,9 @@ class SignalThread(QThread):
 
 def do_the_thing(qid):
     config = mw.addonManager.getConfig(__name__)
-    openBrowseLink(f"tag:#AK_Step{config['step']}_v{config['AnKing_version']}::#UWorld*::{qid}")
+    str = f"tag:#AK_Step{config['step']}_v{config['AnKing_version']}::#UWorld::*{qid} & tag:#AK*{config['shelf']}"
+    print(str)
+    openBrowseLink(str)
 
 signal_thread = SignalThread()
 signal_thread.qid_set_signal.connect(do_the_thing)
